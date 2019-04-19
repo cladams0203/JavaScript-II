cd735bfdeb6f7368ae81d760f67329124b9beced
cladams <cladams0203@gmail.com>
2019-04-18 21:54:07 -0400

    array-methods.js complete

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,28 +56,74 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+runners.forEach((element) => {
+    fullName.push( `${element.first_name} ${element.last_name}`);
+});
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
+runners.map((element) => {
+    allCaps.push(element.first_name.toUpperCase());
+});
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
+largeShirts = runners.filter((element) => {
+    return element.shirt_size == "L";
+});
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
+ticketPriceTotal = runners.reduce((total, element) => {
+    return total += element.donation;
+}, 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+// A thank you for your donation message to the company representitive.
+runners.forEach((element) => {
+    console.log( `We would like to thank ${element.first_name} ${element.last_name} of ${element.company_name} for their \$${element.donation} donation!`);
+});
 
 // Problem 2
+// Average donation ammount.
+let averageDonation = [];
+averageDonation = runners.reduce((total, element) => {
+    return total += element.donation;
+}, 0);
+averageDonation = averageDonation / runners.length;
+console.log(averageDonation);
 
 // Problem 3
+// Shirts to order for the event.
+let xs = runners.filter((element) => {
+    return element.shirt_size === `XS`;
+  });
+  let s = runners.filter((element) => {
+    return element.shirt_size === `S`;
+  });
+  let m = runners.filter((element) => {
+    return element.shirt_size === `M`;
+  });
+  let l = runners.filter((element) => {
+    return element.shirt_size === `L`;
+  });
+  let xl = runners.filter((element) => {
+    return element.shirt_size === `XL`;
+  });
+  let xxl = runners.filter((element) => {
+    return element.shirt_size === `2XL`;
+  });
+  let xxxl = runners.filter((element) => {
+    return element.shirt_size === `3XL`;
+  });
+  console.log(`XS:${xs.length}, S:${s.length}, M:${m.length}, L:${l.length}, XL:${xl.length}, 2XL:${xxl.length}, 3XL:${xxxl.length}`);
