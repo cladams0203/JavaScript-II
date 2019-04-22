cd0da0f843264314618cbd39594a5783b56549a0
cladams <cladams0203@gmail.com>
2019-04-22 18:41:22 -0400

    canged last

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -35,8 +35,8 @@ getLength(items, (length) => {
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
-  return cb(arr.pop());
-}
+  return cb(arr[arr.length - 1]);
+};
 last(items, (end) => {
   console.log(end);
 });
